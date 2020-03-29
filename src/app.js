b1d999c0736553cdbb4891601578b6e9881ac895
Matan Banner <matan.banner@gmail.com>
2020-03-29 18:56:00 +0300

    Styling

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import IndecisionApp from './components/IndecisionApp'
+// standradise the view across all browsers 
+import 'normalize.css/normalize.css'
+import './styles/styles.scss'
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,13 @@ module.exports = {
             test: /\.js$/,
             exclude: /node_modules/
 
+        }, {
+            test: /\.?css$/,
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+            ]
         }]
     },
     devtool: 'cheap-module-eval-source-map',
